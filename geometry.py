a22e0367ae20be7de5f11e0be39d60eda8e06e87
Breadumi <james.allred800@gmail.com>
2024-10-05 03:37:38 -0500

    added basic cell drawing functionality

--- geometry.py
+++ geometry.py
@@ -17,3 +17,37 @@ class Line():
         canvas.create_line(
             self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width = 2
         )
+
+class Cell():
+
+    def __init__(self, win, 
+                 has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True
+                 ):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self.__x1 = None
+        self.__x2 = None
+        self.__y1 = None
+        self.__y2 = None
+        self.__win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        fill_color = "black"
+        if self.has_left_wall:
+            line = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(line, fill_color)
+        if self.has_top_wall:
+            line = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(line, fill_color)
+        if self.has_bottom_wall:
+            line = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(line, fill_color)
+        if self.has_right_wall:
+            line = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -5,16 +5,19 @@ def main():
 
     win = Window(800, 600)
 
-    points = []
-    for i in range(100,601,100):
-        points.append(Point(i,i))
+    # points = []
+    # for i in range(100,601,100):
+    #     points.append(Point(i,i))
 
-    lines = []
-    for i in range(0,6,2):
-        lines.append(Line(points[i], points[i+1]))
+    # lines = []
+    # for i in range(0,6,2):
+    #     lines.append(Line(points[i], points[i+1]))
 
-    for line in lines:
-        win.draw_line(line, "black")
+    # for line in lines:
+    #     win.draw_line(line, "black")
+
+    # c = Cell(win, has_bottom_wall=False)
+    # c.draw(100,100,500,500)
 
     win.wait_for_close()
 
